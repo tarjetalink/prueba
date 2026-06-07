44ce08829f27b18d4143dfdc0099bf912248b8ed
tarjetalink <ferpaisa2010@gmail.com>
2026-06-07 16:37:06 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const API =
+"TU_URL";
+
+const parametros =
+new URLSearchParams(window.location.search);
+
+const id =
+parametros.get("id");
+
+fetch(`${API}?id=${id}`)
+
+.then(r=>r.json())
+
+.then(data=>{
+
+document.getElementById("nombre")
+.innerText=data.nombre;
+
+document.getElementById("visitas")
+.innerText=data.visitas;
+
+let porcentaje =
+(data.visitas/10)*100;
+
+document.getElementById("progreso")
+.style.width=
+porcentaje+"%";
+
+if(data.visitas >= 10){
+
+document.getElementById("mensaje")
+.innerHTML=
+"🎉 Tienes una hamburguesa GRATIS";
+
+}else{
+
+document.getElementById("mensaje")
+.innerHTML=
+`Te faltan ${10-data.visitas} compras`;
+
+}
+
+});
